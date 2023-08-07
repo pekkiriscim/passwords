e098c63f7183b3c0e94a99e300daba4bd97183de
Mustafa Pekkirişci <pekkiriscim@gmail.com>
2023-08-07 16:35:39 +0300

    feat: dynamically change header title based on category filter

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,36 @@
-function Header({ title, description }) {
+import { useContext } from "react";
+
+import { FilterContext } from "@/app/page";
+
+import { useTranslation } from "react-i18next";
+
+function Header() {
+  const { filter } = useContext(FilterContext);
+
+  const { t } = useTranslation();
+
+  const passwordTypes = {
+    passwords: t("dashboard.header.password_types.passwords"),
+    webLogin: t("dashboard.header.password_types.webLogin"),
+    creditCard: t("dashboard.header.password_types.creditCard"),
+    identificationDocument: t(
+      "dashboard.header.password_types.identificationDocument"
+    ),
+    note: t("dashboard.header.password_types.note"),
+    socialMediaAccount: t("dashboard.header.password_types.socialMediaAccount"),
+    emailAccount: t("dashboard.header.password_types.emailAccount"),
+    wiFiPassword: t("dashboard.header.password_types.wiFiPassword"),
+    bankAccount: t("dashboard.header.password_types.bankAccount"),
+  };
+
   return (
     <div>
-      <h2 className="text-2xl font-semibold tracking-tight mb-1">{title}</h2>
-      <p className="text-sm text-muted-foreground">{description}</p>
+      <h2 className="text-2xl font-semibold tracking-tight mb-1">
+        {passwordTypes[filter.passwordType]}
+      </h2>
+      <p className="text-sm text-muted-foreground">
+        {t("dashboard.header.description")}
+      </p>
     </div>
   );
 }
--- components/Page/Dashboard.jsx
+++ components/Page/Dashboard.jsx
@@ -26,12 +26,7 @@ function Dashboard() {
       <div className="w-full h-full grid grid-cols-[17.5rem_1fr]">
         <Sidebar />
         <div className="w-full h-full px-8 py-6 overflow-auto">
-          <Header
-            title={"Şifreler"}
-            description={
-              "Şifrelerinizi güvenli bir şekilde yönetin ve erişim sağlayın."
-            }
-          />
+          <Header />
           <SearchBar />
           <Passwords />
         </div>
